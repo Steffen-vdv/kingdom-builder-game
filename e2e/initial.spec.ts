4144e65eb53ac817e81f722feb93b468e9e18e38
Steffen van der Vaart <46325162+Steffen-vdv@users.noreply.github.com>
2025-08-28 19:41:22 +0200

    test(web): update e2e to match testlab header

--- e2e/initial.spec.ts
+++ e2e/initial.spec.ts
@@ -2,7 +2,5 @@ import { test, expect } from '@playwright/test';
 
 test('renders initial screen', async ({ page }) => {
   await page.goto('/');
-  await expect(
-    page.getByRole('heading', { name: 'Hello World' }),
-  ).toBeVisible();
+  await expect(page.getByRole('heading', { name: /testlab/i })).toBeVisible();
 });
