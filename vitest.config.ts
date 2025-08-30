8df7db34f93b4a87b04b9118c4f547e66aa32c11
Steffen van der Vaart <46325162+Steffen-vdv@users.noreply.github.com>
2025-08-31 01:03:18 +0200

    test: add web panel tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,12 @@
+import path from 'path';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@kingdom-builder/engine': path.resolve(__dirname, 'packages/engine/src'),
+    },
+  },
   test: {
     exclude: ['**/node_modules/**'],
     coverage: {
