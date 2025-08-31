cabaa9195bed84ec9bf6385bfe7ce2be588e3e6e
Steffen van der Vaart <46325162+Steffen-vdv@users.noreply.github.com>
2025-08-31 17:12:12 +0200

    feat: hide untouched zero stats

--- packages/engine/src/effects/stat_remove.ts
+++ packages/engine/src/effects/stat_remove.ts
@@ -4,6 +4,7 @@ import type { StatKey } from '../state';
 export const statRemove: EffectHandler = (effect, ctx, mult = 1) => {
   const key = effect.params!['key'] as StatKey;
   const amount = effect.params!['amount'] as number;
-  ctx.activePlayer.stats[key] =
-    (ctx.activePlayer.stats[key] || 0) - amount * mult;
+  const newVal = (ctx.activePlayer.stats[key] || 0) - amount * mult;
+  ctx.activePlayer.stats[key] = newVal;
+  if (newVal !== 0) ctx.activePlayer.statsHistory[key] = true;
 };
