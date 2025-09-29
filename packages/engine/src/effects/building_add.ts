a8be9cf2423295f17d4e122b7566601d43f85a52
Steffen van der Vaart <46325162+Steffen-vdv@users.noreply.github.com>
2025-09-29 19:03:30 +0200

    Prevent rebuilding already owned structures

--- packages/engine/src/effects/building_add.ts
+++ packages/engine/src/effects/building_add.ts
@@ -1,8 +1,12 @@
 import type { EffectHandler, EffectCostCollector } from '.';
 
 export const buildingAdd: EffectHandler = (effect, ctx, mult = 1) => {
-  const id = effect.params!['id'] as string;
-  for (let index = 0; index < Math.floor(mult); index++) {
+  const id = effect.params?.['id'] as string;
+  if (!id) throw new Error('building:add requires id');
+  const iterations = Math.floor(mult);
+  for (let index = 0; index < iterations; index++) {
+    if (ctx.activePlayer.buildings.has(id))
+      throw new Error(`Building ${id} already built`);
     ctx.activePlayer.buildings.add(id);
     const building = ctx.buildings.get(id);
     if (building.onBuild)
