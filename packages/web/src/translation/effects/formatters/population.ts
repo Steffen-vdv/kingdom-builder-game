7e4c35879a70133cac9a19d21a385990421a6cb6
Steffen van der Vaart <46325162+Steffen-vdv@users.noreply.github.com>
2025-10-04 12:08:40 +0200

    Ensure population descriptions include icons

--- packages/web/src/translation/effects/formatters/population.ts
+++ packages/web/src/translation/effects/formatters/population.ts
@@ -1,44 +1,55 @@
 import { POPULATION_ROLES, POPULATION_INFO } from '@kingdom-builder/contents';
 import { registerEffectFormatter } from '../factory';
+import { resolvePopulationDisplay } from '../helpers';
 
 registerEffectFormatter('population', 'add', {
-  summarize: (eff) => {
-    const role = eff.params?.['role'] as
-      | keyof typeof POPULATION_ROLES
-      | undefined;
-    const icon = role
-      ? POPULATION_ROLES[role]?.icon || role
-      : POPULATION_INFO.icon;
-    return `${POPULATION_INFO.icon}(${icon}) +1`;
-  },
-  describe: (eff) => {
-    const role = eff.params?.['role'] as
-      | keyof typeof POPULATION_ROLES
-      | undefined;
-    const info = role ? POPULATION_ROLES[role] : undefined;
-    const label = info?.label || role || POPULATION_INFO.label;
-    const icon = info?.icon || '';
-    return `Add ${icon} ${label}`;
-  },
+	summarize: (eff) => {
+		const role = eff.params?.['role'] as
+			| keyof typeof POPULATION_ROLES
+			| undefined;
+		const icon = role
+			? POPULATION_ROLES[role]?.icon || role
+			: POPULATION_INFO.icon;
+		return `${POPULATION_INFO.icon}(${icon}) +1`;
+	},
+	describe: (eff) => {
+		const role = eff.params?.['role'] as
+			| keyof typeof POPULATION_ROLES
+			| undefined;
+		const { icon, label } = resolvePopulationDisplay(role);
+		return `Add ${icon} ${label}`;
+	},
+	log: (eff) => {
+		const role = eff.params?.['role'] as
+			| keyof typeof POPULATION_ROLES
+			| undefined;
+		const { icon, label } = resolvePopulationDisplay(role);
+		return `Added ${icon} ${label}`;
+	},
 });
 
 registerEffectFormatter('population', 'remove', {
-  summarize: (eff) => {
-    const role = eff.params?.['role'] as
-      | keyof typeof POPULATION_ROLES
-      | undefined;
-    const icon = role
-      ? POPULATION_ROLES[role]?.icon || role
-      : POPULATION_INFO.icon;
-    return `${POPULATION_INFO.icon}(${icon}) -1`;
-  },
-  describe: (eff) => {
-    const role = eff.params?.['role'] as
-      | keyof typeof POPULATION_ROLES
-      | undefined;
-    const info = role ? POPULATION_ROLES[role] : undefined;
-    const label = info?.label || role || POPULATION_INFO.label;
-    const icon = info?.icon || '';
-    return `Remove ${icon} ${label}`;
-  },
+	summarize: (eff) => {
+		const role = eff.params?.['role'] as
+			| keyof typeof POPULATION_ROLES
+			| undefined;
+		const icon = role
+			? POPULATION_ROLES[role]?.icon || role
+			: POPULATION_INFO.icon;
+		return `${POPULATION_INFO.icon}(${icon}) -1`;
+	},
+	describe: (eff) => {
+		const role = eff.params?.['role'] as
+			| keyof typeof POPULATION_ROLES
+			| undefined;
+		const { icon, label } = resolvePopulationDisplay(role);
+		return `Remove ${icon} ${label}`;
+	},
+	log: (eff) => {
+		const role = eff.params?.['role'] as
+			| keyof typeof POPULATION_ROLES
+			| undefined;
+		const { icon, label } = resolvePopulationDisplay(role);
+		return `Removed ${icon} ${label}`;
+	},
 });
