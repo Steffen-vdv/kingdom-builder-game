6036a03a916c3ee896dfadc6a00a996d745db142
Steffen van der Vaart <46325162+Steffen-vdv@users.noreply.github.com>
2025-10-11 17:26:58 +0200

    Fix Tailwind content configuration

--- packages/web/tailwind.config.cjs
+++ packages/web/tailwind.config.cjs
@@ -1,9 +1,13 @@
+const path = require('node:path');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	darkMode: 'class',
 	content: {
-		relative: true,
-		files: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+		files: [
+			path.resolve(__dirname, 'index.html'),
+			path.resolve(__dirname, 'src/**/*.{js,ts,jsx,tsx}'),
+		],
 	},
 	theme: { extend: {} },
 	plugins: [],
