4e04bbe710cc37dc165d19273fcb9ae8968a3017
Steffen van der Vaart <46325162+Steffen-vdv@users.noreply.github.com>
2025-10-18 20:49:53 +0200

    Apply suggestion from @Steffen-vdv

--- packages/contents/src/config/builders/effectParams/passiveParams.ts
+++ packages/contents/src/config/builders/effectParams/passiveParams.ts
@@ -56,7 +56,7 @@ type PassiveEffectParams = {
 } & PassiveEffectTriggerMap;
 // prettier-ignore
 export class PassiveEffectParamsBuilder extends ParamsBuilder<
-        PassiveEffectParams
+	PassiveEffectParams
 > {
 	private triggerBuckets = new Map<TriggerKey, EffectConfig[]>();
 	id(id: string) {
