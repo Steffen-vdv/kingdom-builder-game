6199bf05528cfbf7927b3afffd0fb80a9fd440a4
Steffen van der Vaart <46325162+Steffen-vdv@users.noreply.github.com>
2025-10-23 18:18:49 +0200

    Add ResourceV2 change builder helpers

---
+++ packages/contents/src/resourceV2/effects/index.ts
@@ -0,0 +1,4 @@
+export { resourceChange } from './changeBuilder';
+export type { ResourceChangeBuilder, ResourceChangeEffectParams } from './changeBuilder';
+
+export type { ResourceChangeParameters, ResourceChangeRoundingMode, ResourceReconciliationMode, ResourceAmountChangeParameters, ResourcePercentChangeParameters } from './types';
